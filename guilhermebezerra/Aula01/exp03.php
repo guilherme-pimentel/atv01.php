6c0e9915eb09c4328b985fe4857cf3962a5302ae
Guilhermepimentel <145464569+guilherme-pimentel@users.noreply.github.com>
2023-11-07 11:31:31 -0300

    Add files via upload

---
+++ guilhermebezerra/Aula01/exp03.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exp03</title>
+</head>
+<body>
+    <?php
+     ECHO"mello world <br>";
+     echo"mello world <br>";
+     ECHO"mello world <br>";
+    ?>
+</body>
+</html>
---
+++ guilhermebezerra/pratica/clara.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+// o echo faz impressao de algo
+echo "impressao em php";
+
+echo "<br>";
+
+// uma funcao que imprime 
+print_r("isto e um teste");
+
+/*
+echo " impressao em php ";
+echo " imoressao em php ";
+echo " impressao em php ";
+*/ 
+ 
+?>
